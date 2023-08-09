0334765118b1457faf180f0691dcebdab698b247
David Kittle <david_kittle@selinc.com>
2023-08-08 17:03:28 -0700

    Update main.go

--- bletchley/main.go
+++ bletchley/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"flag"
+	"log"
+	"os"
+)
 
+func main() {
+	rleFilePath := flag.String("path", "attachments/PC-POW-corrected.txt", "path to the input Run Length Encoded file")
+	flag.Parse()
+
+	rawRLE := getFileContents(*rleFilePath)
+}
+
+func getFileContents(path string) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(errors.Join())
+	}
 }
